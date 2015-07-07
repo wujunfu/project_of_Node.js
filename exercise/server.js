1a787b5e6095f4290fd539189495291f7c21d2a0
wujunfu [吴军甫] <wujunfu@xiaomi.com>
2015-07-07 15:19:29 +0800

    update

--- exercise/server.js
+++ exercise/server.js
@@ -3,14 +3,13 @@ var url = require("url");
 
 function start(route, handle){
     function onRequest(request, response){
+        var postdata = "";
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " +pathname+ " received.");
-        route(handle, pathname);
-    
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write("Hello World!");
-        response.end();
+        
+        route(handle, pathname, response, request);
     }
+
     http.createServer(onRequest).listen(8887);
     console.log("Server has started.");
 }
